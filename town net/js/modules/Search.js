73ed22185697e53d3044f979c431d1a4c83a1305
DanBadRobot <danmikhael@gmail.com>
2018-07-11 16:35:21 +1000

    Search.js file updated with keyCode checks for overlay - Section 13 lesson 51

--- town net/js/modules/Search.js
+++ town net/js/modules/Search.js
@@ -1,3 +1,6 @@
+
+import $ from 'jquery';
+
 class Search {
 
   // 1. describer and initiate our Search object
@@ -6,21 +9,38 @@ class Search {
     this.closeButton = $(".search-overlay__close");
     this.searchOverlay = $(".search-overlay");
     this.events();
+    this.isOverlayOpen = false;
   }
 
   // 2. events
   events() {
     this.openButton.on("click", this.openOverlay.bind(this)); // open overlay event.
     this.closeButton.on("click", this.closeOverlay.bind(this)); // close overlay event
+    $(document).on("keyup", this.keyPressDispatcher.bind(this));
   }
 
   // 3. methods
+
+  keyPressDispatcher(e) {
+    if(e.keyCode == 83 && !this.isOverlayOpen) {
+    this.openOverlay();
+    }
+
+    if (e.keyCode == 27 && this.isOverlayOpen) {
+      this.closeOverlay();
+    }
+  }
+
   openOverlay() { // open the Overlay function
     this.searchOverlay.addClass("search-overlay--active"); // adds the active overalay class
+    $("body").addClass("body-no-scroll");
+    this.isOverlayOpen = true;
   }
 
   closeOverlay() {
     this.searchOverlay.removeClass("search-overlay--active"); // removes the overlay class
+    $("body").removeClass("body-no-scroll");
+    this.isOverlayOpen = false;
   }
 }
 
