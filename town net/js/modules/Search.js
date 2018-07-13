05c49f85e5d4fc873ce2221a9beb36d2f8db5cce
DanBadRobot <danmikhael@gmail.com>
2018-07-13 17:07:50 +1000

    updated Search.js with lesson 55 generating HTML based on JSON

--- town net/js/modules/Search.js
+++ town net/js/modules/Search.js
@@ -47,8 +47,14 @@ class Search {
   }
 
   getResults() {
-    this.resultsDiv.html("real search results here");
-    this.isSpinnerVisible = false;
+    $.getJSON('http://dans-mbp.gateway:5757/wp-json/wp/v2/posts?search=' = this.searchField.val(), posts => {
+    this.resultsDiv.html(`
+      <h2 class="search-overlay__section-title">General Information</h2>
+      <ul class="link-list min-list">
+      ${posts.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
+      </ul>
+      `);
+  });
   }
 
   keyPressDispatcher(e) {
