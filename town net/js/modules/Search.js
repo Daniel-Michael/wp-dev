45b2da4708a71bd37fcca0cd909cb0a0962399f8
DanBadRobot <danmikhael@gmail.com>
2018-07-24 16:39:46 +1000

    updated Search file

--- town net/js/modules/Search.js
+++ town net/js/modules/Search.js
@@ -48,18 +48,21 @@ class Search {
   }
 
   getResults() {
-    $.getJSON(townData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val(), posts => {
-    $.getJSON(townData.root_url + '/wp-json/wp/v2/pagess?search=' + this.searchField.val(), pages => {
-      var combinedResults = posts.concat(pages);
-      this.resultsDiv.html(`
-      <h2 class="search-overlay__section-title">General Information</h2>
-      ${combinedResults.length ? '<ul class="link-list min-list">' : '<p>No Search results</p>'}
-      ${combinedResults.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
-      ${combinedResults.length ? '</ul>' : ''}
-      `);
-      this.isSpinnerVisible = false;
-    });
-  });
+    $.when(
+      $.getJSON(townData.root_url + '/wp-json/wp/v2/posts?search=' + this.searchField.val()),
+      $.getJSON(townData.root_url + '/wp-json/wp/v2/pages?search=' + this.searchField.val())
+      ).then((posts, pages)=>);  
+    var combinedResults = posts[0].concat(pages[0]);
+    this.resultsDiv.html(`
+    <h2 class="search-overlay__section-title">General Information</h2>
+    ${combinedResults.length ? '<ul class="link-list min-list">' : '<p>No Search results</p>'}
+    ${combinedResults.map(item => `<li><a href="${item.link}">${item.title.rendered}</a></li>`).join('')}
+    ${combinedResults.length ? '</ul>' : ''}
+    `);
+    this.isSpinnerVisible = false;
+   },  () => {
+     this.resultsDiv.html('<p>Unexpected error, please try again</p>');
+   });
   }
 
   keyPressDispatcher(e) {
